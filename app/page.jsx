5a1828b48f4c32bd102f441d22d6e57cf3ac3bff
RajAditya7777 <aditya.raj88005@gmail.com>
2025-11-04 23:38:44 +0530

    Added Navbar

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Footer from "./_components/Footer";
+import Hero from "./_components/Hero";
+import Navbar from "./_components/Navbar";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </main>
+  );
+}
+
