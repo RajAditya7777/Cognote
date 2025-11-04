36e0db0e63dbc14b31074fd834f2a2a67d3426e1
RajAditya7777 <aditya.raj88005@gmail.com>
2025-11-05 00:06:23 +0530

    2: added hero bg

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,9 +1,23 @@
+import Ballpit from '@/components/Ballpit'
 import React from 'react'
 
 const Hero = () => {
   return (
     <div>
-        
+      <div style={{ position: 'relative', overflow: 'hidden', minHeight: '960px', maxHeight: '960px', width: '100%' }}>
+        <Ballpit
+          count={120}
+          gravity={0.01}
+          friction={0.9975}
+          wallBounce={0.95}
+          followCursor={false}
+          colors={[
+            
+            '#FFFFFF',  // White
+            '#0000FF',  // Blue
+          ]}
+        />
+      </div>
     </div>
   )
 }
